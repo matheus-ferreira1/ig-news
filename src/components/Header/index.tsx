a81e8066f43d852fd3c54b0399509532ac58f2d5
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-03 00:36:04 -0300

    adicionado o componente que monitora qual pagina esta ativa e acrescenta classe no link ativo para melhor usabilidade do site

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,25 @@
+import { ActiveLink } from '../ActiveLink'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
 
+//A utilização da tag LINK do Next faz com que o carregamento das paginas seja muito
+//mais rápido, pois o next irá recarregar apenas o conteúdo principal da pagina.
+//Execução do conceito de SPA.
+
 export function Header() {
+    
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClass={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClass={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
@@ -16,3 +27,6 @@ export function Header() {
         </header>
     )
 }
+
+//Uma forma dinamica de se colocar a classe no link ativo é usando condicional:
+//<a className={asPath == '/posts' ? styles.active : ''}>Posts</a>
